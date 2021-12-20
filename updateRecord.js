0ec71e1fc7ca46620fc8124becf37957873ed161
Charlie C. Wang <32430037+CRE0809@users.noreply.github.com>
2021-12-21 04:11:47 +0800

    Update updateRecord.js

--- updateRecord.js
+++ updateRecord.js
@@ -1,17 +1,17 @@
 var request = require('request');
 
 //Required
-const ZONE_ID = '';
-const USER_EMAIL = '';
-const AUTH_KEY = '';
-const IDENTIFIER = '';
+const ZONE_ID = "";
+const USER_EMAIL = "";
+const AUTH_KEY = "";
+const IDENTIFIER = "";
 
 //DNS Data
-const TYPE = ''; //valid values: A, AAAA, CNAME, HTTPS, TXT, SRV, LOC, MX, NS, CERT, DNSKEY, DS, NAPTR, SMIMEA, SSHFP, SVCB, TLSA, URI
-const NAME = ''; //max length: 255 , example : www.example.com
-const CONTENT = ''; //Example: 127.0.0.1
-const TTL = '1'; //Must be between 60 and 86400, or 1 for 'automatic'
-const PROXIED = 'true';  //true or false
+const TYPE = ""; //valid values: A, AAAA, CNAME, HTTPS, TXT, SRV, LOC, MX, NS, CERT, DNSKEY, DS, NAPTR, SMIMEA, SSHFP, SVCB, TLSA, URI
+const NAME = ""; //max length: 255 , example : www.example.com
+const CONTENT = ""; //Example: 127.0.0.1
+const TTL = 1; //Must be between 60 and 86400, or 1 for 'automatic'
+const PROXIED = true;  //true or false
 
 //Code
 var options = {
